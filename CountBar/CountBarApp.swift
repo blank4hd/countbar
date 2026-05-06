4816d462b7a31d300fb2606cb08f6f558b2450a4
Chanakya <chanakya@terpmail.umd.edu>
2026-05-06 13:20:50 -0400

    Initial Commit

---
+++ CountBar/CountBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountBarApp.swift
+//  CountBar
+//
+//  Created by Chanakya Chelamkuri on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct CountBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
